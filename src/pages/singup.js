92bbae7d927cb47c9fe2166d1799ec090f6250ff
David Basto <davidbasto01@gmail.com>
2022-04-06 18:47:28 -0500

    [UPDATE] sign up styles

--- src/pages/singup.js
+++ src/pages/singup.js
@@ -1,35 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { createCustomerSchema } from '@schemas/customerSchema';
+import { addCustomer } from '@services/api/customer.js';
 import styles from '@styles/CreateAccount.module.scss';
-import lockImage from '@images/lock.png'
+import lockImage from '@images/lock.png';
+import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const CreateAccount = () => {
-	return (
-		<>	
-		<Head>
-			<title>Next Shop - Create Account</title>
-		</Head>
-			<div className={styles["CreateAccount"]}>
-				<div className={styles["Login-header"]}>
-					<h2>Crear Cuenta</h2>
-					<img src={lockImage.src}/>
-				</div>
-				<div className={styles["CreateAccount-container"]}>
-					<form action="/" className={styles["form"]}>
-						<div>
-							<label for="name" className={styles["label"]}>Name</label>
-							<input type="text" id="name" placeholder="Name" className={styles["input"] + " " + styles["input-name"]} />
-							<label for="email" className={styles["label"]}>Email</label>
-							<input type="text" id="email" placeholder="correo@example.com" className={styles["input"] + " " + styles["input-email"]} />
-							<label for="password" className={styles["label"]}>Password</label>
-							<input type="password" id="password" placeholder="*********" className={styles["input"] + " " + styles["input-password"]} />
-						</div>
-						<input type="submit" value="Create" className={styles["create-button"] + " " + "primary-button" } />
-					</form>
-				</div>
-			</div>
-		</>
-	);
-}
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: joiResolver(createCustomerSchema),
+  });
+
+  const [emailError, setEmailError] = useState(null);
+
+  const onSubmit = (data) => {
+    addCustomer(data)
+      .then((res) => {
+        alert('Successful user registration!')
+		router.push('/login');
+      })
+      .catch((err) => {
+        if (err.response.status === 409) setEmailError('email already exists');
+      });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Next Shop - Create Account</title>
+      </Head>
+      <div className={styles['CreateAccount']}>
+        <div className={styles['Login-header']}>
+          <h2>Crear Cuenta</h2>
+          <img src={lockImage.src} />
+        </div>
+        <div className={styles['CreateAccount-container']}>
+          <form onSubmit={handleSubmit(onSubmit)} className={styles['form']}>
+            <div>
+              <div className={styles['form-item']}>
+                <label htmlFor="name" className={styles['label']}>
+                  Name
+                </label>
+                <input {...register('name')} type="text" id="name" placeholder="Name" className={styles['input'] + ' ' + styles['input-name']} />
+                <p>{errors?.name?.message}</p>
+              </div>
+
+              <div className={styles['form-item']}>
+                <label htmlFor="lastName" className={styles['label']}>
+                  Last name
+                </label>
+                <input {...register('lastName')} type="text" id="lastName" placeholder="last name" className={styles['input'] + ' ' + styles['input-name']} />
+                <p>{errors?.lastName?.message}</p>
+              </div>
+
+              <div className={styles['form-item']}>
+                <label htmlFor="phone" className={styles['label']}>
+                  Phone
+                </label>
+                <input {...register('phone')} type="text" id="phone" placeholder="phone" className={styles['input'] + ' ' + styles['input-name']} />
+                <p>{errors?.phone?.message}</p>
+              </div>
+
+              <div className={styles['form-item']}>
+                <label htmlFor="email" className={styles['label']}>
+                  Email
+                </label>
+                <input {...register('user.email')} type="text" id="email" placeholder="correo@example.com" className={styles['input'] + ' ' + styles['input-email']} />
+                {emailError && <p>{emailError}</p>}
+                <p>{errors?.user?.email?.message}</p>
+              </div>
+
+              <div className={styles['form-item']}>
+                <label htmlFor="password" className={styles['label']}>
+                  Password
+                </label>
+                <input {...register('user.password')} type="password" id="password" placeholder="*********" className={styles['input'] + ' ' + styles['input-password']} />
+                <p>{errors?.user?.password?.message}</p>
+              </div>
+            </div>
+            <input type="submit" value="Create" className={styles['create-button'] + ' ' + 'primary-button'} />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (session && session.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
 
 export default CreateAccount;
