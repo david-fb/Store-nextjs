f1c9c7f8f8f3db83f128e2e1c72e5ad7ccb66939
David Basto <davidbasto01@gmail.com>
2022-04-07 13:03:29 -0500

    [FIX] Link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '@styles/Menu.module.scss';
 import { signOut } from 'next-auth/react';
 
-
 const Menu = () => {
-	return (
-		<div className={styles.Menu}>
-			<a href="/orders" className={styles["Menu-item"]}>My orders</a>
-			<a href="/account" className={styles["Menu-item"]}>My account</a>
-			<button onClick={signOut} className={styles["Menu-item"]}>Sign out</button>
-		</div>
-	);
-}
+  return (
+    <div className={styles.Menu}>
+      <Link href="/orders">
+        <a className={styles['Menu-item']}>
+          My orders
+        </a>
+      </Link>
+      <Link href="/account">
+        <a className={styles['Menu-item']}>
+          My account
+        </a>
+      </Link>
+      <button onClick={signOut} className={styles['Menu-item']}>
+        Sign out
+      </button>
+    </div>
+  );
+};
 
 export default Menu;
