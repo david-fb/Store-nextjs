f0763be01f8164a5d2e1165f7a6952011e55834d
David Basto <davidbasto01@gmail.com>
2022-04-16 22:36:25 -0500

    [ADD] image on mouseover zoom

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,10 +1,9 @@
 import endPoints from '@services/api';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import Head from 'next/head';
 import axios from 'axios';
 import SimpleHeader from '@common/SimpleHeader';
 import styles from '@styles/Product.module.scss';
-import addToCart from '@icons/bt_add_to_cart.svg';
 import AppContext from '@context/AppContext';
 
 export default function Product({ product }) {
@@ -15,6 +14,74 @@ export default function Product({ product }) {
       addToCart(item);
     }
   };
+  const resultRef = useRef(null);
+  const lensRef = useRef(null);
+  const imgRef = useRef(null);
+  const handleZoom = (e) => {
+    lensRef.current.style.display = 'block';
+    resultRef.current.style.display = 'block';
+    let result = resultRef.current;
+    let lens = lensRef.current;
+    let img = imgRef.current;
+
+    /*prevent any other actions that may occur when moving over the image:*/
+    e.preventDefault();
+
+    /*calculate the ratio between result DIV and lens:*/
+    let cx = result.offsetWidth / lens.offsetWidth;
+    let cy = result.offsetHeight / lens.offsetHeight;
+
+    result.style.backgroundImage = "url('" + img.src + "')";
+    result.style.backgroundSize = img.width * cx + 'px ' + img.height * cy + 'px';
+
+    let pos, x, y;
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    /*calculate the position of the lens:*/
+    x = pos.x - lens.offsetWidth / 2;
+    y = pos.y - lens.offsetHeight / 2;
+    /*prevent the lens from being positioned outside the image:*/
+    if (x > img.width - lens.offsetWidth) {
+      x = img.width - lens.offsetWidth;
+    }
+    if (x < 0) {
+      x = 0;
+    }
+    if (y > img.height - lens.offsetHeight) {
+      y = img.height - lens.offsetHeight;
+    }
+    if (y < 0) {
+      y = 0;
+    }
+    /*set the position of the lens:*/
+    lens.style.left = x + 'px';
+    lens.style.top = y + 'px';
+    /*display what the lens "sees":*/
+    result.style.backgroundPosition = '-' + x * cx + 'px -' + y * cy + 'px';
+
+    function getCursorPos(e) {
+      let a,
+        x = 0,
+        y = 0;
+      e = e || window.event;
+      /*get the x and y positions of the image:*/
+      a = img.getBoundingClientRect();
+      /*calculate the cursor's x and y coordinates, relative to the image:*/
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+      /*consider any page scrolling:*/
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return { x: x, y: y };
+    }
+  };
+
+  const handleMouseLeave = (e) => {
+    e.preventDefault();
+    lensRef.current.style.display = 'none';
+    resultRef.current.style.display = 'none';
+  }
+
   return (
     <>
       <Head>
@@ -23,9 +90,10 @@ export default function Product({ product }) {
       <SimpleHeader color={'skyblue'} />
       <div className={styles['Product-container']}>
         <div className={styles['Product-content']}>
-          <div className={styles['Product-Image']}>
-            <img src={product.image} alt={product.name} />
-          </div>
+          <figure className={styles['Product-Image']}>
+            <div ref={lensRef} onMouseMove={handleZoom} onMouseLeave={handleMouseLeave} className={styles['img-zoom-lens']}></div>
+            <img ref={imgRef} onMouseMove={handleZoom} src={product.image} alt={product.name} />
+          </figure>
           <div className={styles['Product-info']}>
             <h2>{product.name}</h2>
             <p className={styles['Price']}>${product.price}</p>
@@ -35,6 +103,7 @@ export default function Product({ product }) {
             </button>
             <h3>Description</h3>
             <p className={styles['Description']}>{product.description}</p>
+            <div ref={resultRef} className={styles['img-zoom-result']}></div>
           </div>
         </div>
       </div>
