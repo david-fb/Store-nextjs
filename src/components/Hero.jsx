3951a4c7569020c87556743f6628e628f8fe98e1
David Basto <davidbasto01@gmail.com>
2022-04-25 18:19:51 -0500

    [UPDATE] implement next Image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import PawPatrolImage from '@images/PawPatrol2.png';
 import styles from '@styles/Hero.module.scss';
 
@@ -5,18 +6,19 @@ export default function Hero({ title, info, color, image, hasButton, textButton 
   if (!color) color = 'blue';
   if (!title) title = 'Default title';
   if (!info) info = 'Lorem Ipsum';
-  if (!image) image = PawPatrolImage.src;
-  if (!hasButton) hasButton = false;
+  if (!image) image = PawPatrolImage;
   if (!textButton) textButton = 'MORE DETAILS'
   return (
     <div className={styles['header-container'] + ' ' + styles[`bg-${color}`]}>
       <div className={styles['header-content']}>
         <div className={styles['content-text']}>
-          <h2>{title}</h2>
+          <h1>{title}</h1>
           <p>{info}</p>
           {hasButton && <button className="primary-button">{textButton}</button>}
         </div>
-        <img src={image} />
+        <figure>
+          <Image src={image} layout='fill' priority objectFit='scale-down'/>
+        </figure>
       </div>
     </div>
   );
