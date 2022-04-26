24b56341024dccb3ee7a58b9e5bc22a1fa981de8
David Basto <davidbasto01@gmail.com>
2022-04-25 20:09:08 -0500

    [UPDATE] implement next Image

--- src/common/SimpleHeader/index.js
+++ src/common/SimpleHeader/index.js
@@ -1,10 +1,11 @@
 import styles from './index.module.scss';
+import Image from 'next/image';
 
 export default function ({title, image, color}) {
   return (
     <div className={styles['header-container'] + " " + `${color ? styles[`bg-${color}`] : ''}`}>
-      {title && <h2>{title}</h2>}
-      {image && <img src={image.src} />}
+      {title && <h1>{title}</h1>}
+      {image && <figure><Image src={image} layout='fill' priority objectFit='scale-down'/></figure>}
     </div>
   );
 }
