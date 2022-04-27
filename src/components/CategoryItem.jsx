fb8a09147cdb922cc06532385fa4681c8021307e
David Basto <davidbasto01@gmail.com>
2022-04-27 15:20:20 -0500

    [UPDATE] implement next image

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@styles/CategoryItem.module.scss';
 
-const CategoryItem = ({category}) => {
-    const links = {
-        Maternidad: '/care-and-maternity',
-        Ropa: '/clothes',
-        Juguetes: '/toys',
-    }
-    return (
-        <div className={styles.CategoryItem}>
-            <figure className={styles["image-container"]}>
-                <img src={category.image} />
-            </figure>
-            <div className={styles["info-container"]}>
-                <p>{category.name}</p>
-                <Link href={`${links[category.name]}`}>
-                    <a className={`primary-button`}>Go</a>
-                </Link>
-            </div>
-        </div>
-    );
-}
+const CategoryItem = ({ category }) => {
+  const links = {
+    Maternidad: '/care-and-maternity',
+    Ropa: '/clothes',
+    Juguetes: '/toys',
+  };
+  return (
+    <div className={styles.CategoryItem}>
+      <figure className={styles['image-container']}>
+        <Image src={category.image} layout="fill" priority objectFit="scale-down" alt={`category-image-${category.name}`} />
+      </figure>
+      <div className={styles['info-container']}>
+        <p>{category.name}</p>
+        <Link href={`${links[category.name]}`}>
+          <a href="dummy" className={`primary-button`}>
+            Go
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default CategoryItem;
