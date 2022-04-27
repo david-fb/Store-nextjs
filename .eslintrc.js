b58a18d7642fe9cc343e3f4906a6219c7ca1cae0
David Basto <davidbasto01@gmail.com>
2022-04-27 14:22:33 -0500

    [DELETE] no image rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,5 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
     semi: ['error', 'always'],
-    '@next/next/no-img-element': 'offf',
   },
 };
