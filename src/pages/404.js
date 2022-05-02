eac6a253443e7325f8c8fb82192748b76c858dfb
David Basto <davidbasto01@gmail.com>
2022-05-02 17:30:53 -0500

    [UPDATE] NotFound

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import styles from '@styles/NotFound.module.scss';
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import NotFoundImage from '@images/sad.png';
 
 const NotFound = () => {
-	return (
-		<div>
-			Not Found, error 404.
-		</div>
-	);
-}
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <div className={styles['NotFound']}>
+        <div className={styles['NotFound-container']}>
+          <div className={styles['NotFound-container-message']}>
+            <figure className={styles['NotFound-container-message-figure']}>
+              <Image src={NotFoundImage} alt="Not Found" layout="fill" objectFit="contain" />
+            </figure>
+            <div className={styles['NotFound-container-message-text']}>
+              <h1>404 - PAGE NOT FOUND</h1>
+              <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+            </div>
+          </div>
+          <Link href="/">
+            <a href="dummy" className={`primary-button ${styles['NotFound-container-link']}`}>
+              GO HOME
+            </a>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,16 @@ import useInitialState from '@hooks/useInitialState';
 import { SessionProvider } from 'next-auth/react';
 import '@styles/globals.scss';
 import Footer from '@components/Footer';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const initialState = useInitialState();
+  const router = useRouter();
+
   return (
     <SessionProvider session={session}>
       <AppContext.Provider value={initialState}>
-        <Header />
+        {router.pathname !== '/404' && <Header />}
         <Component {...pageProps} />
         <Footer />
       </AppContext.Provider>
